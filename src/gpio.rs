b0a719ee70e1a8aeadec2c76891e81b8c2c32c4d
joel <joelimgu@gmail.com>
2022-06-06 09:30:08 +0200

    checkpoint for AFIO

--- src/gpio.rs
+++ src/gpio.rs
@@ -101,6 +101,12 @@ impl PinMode for Output<OpenDrain> {
     const PULL: u32 = 0b00;
 }
 
+impl<const i: u8> PinMode for Alternate<i, PushPull> {
+    const MODE: u32 = 0b10;
+    const TYPE: u32 = 0b0;
+    const PULL: u32 = 0b00;
+}
+
 pub struct Pin<const P: char, const N: u8, MODE = Analog> {
     _mode: PhantomData<MODE>,
 }
@@ -129,9 +135,11 @@ pub struct Pin<const P: char, const N: u8, MODE = Analog> {
 
 macro_rules! convert_pin {
     ($input:ty, $output:ty) => {
-        impl<const P: char, const N: u8> From<Pin<P, N, $input>> for Pin<P, N, $output> {
-            fn from(_: Pin<P, N, $input>) -> Self {
-                let gpio = unsafe { &(*Gpio::<P>::ptr()) };
+        impl<const P: char, const N: u8> From<crate::gpio::Pin<P, N, $input>>
+            for crate::gpio::Pin<P, N, $output>
+        {
+            fn from(_: crate::gpio::Pin<P, N, $input>) -> Self {
+                let gpio = unsafe { &(*crate::gpio::Gpio::<P>::ptr()) };
                 unsafe {
                     // Set the mode in the register N*2 as there is two bits to configure
                     gpio.moder.modify(|r, w| {
@@ -255,12 +263,51 @@ impl<const P: char> Gpio<P> {
 //     }
 // }
 
+macro_rules! from_pin {
+    ($port_id:expr, $pin_number:expr, $afi:expr, $input:ty, $mode:ty) => {
+        impl From<super::Pin<$port_id, $pin_number, $input>>
+            for crate::gpio::Pin<$port_id, $pin_number, super::Alternate<$afi, $mode>>
+        {
+            fn from(_: super::Pin<$port_id, $pin_number, $input>) -> Self {
+                let gpio = unsafe { &(*crate::gpio::Gpio::<$port_id>::ptr()) };
+                unsafe {
+                    // Set the mode in the register N*2 as there is two bits to configure
+                    gpio.moder.modify(|r, w| {
+                        w.bits(
+                            (r.bits() & !(0b11 << $pin_number * 2))
+                                | (<super::Alternate<$afi, $mode>>::MODE << $pin_number * 2),
+                        )
+                    });
+                    // Write the config into the type register
+                    gpio.otyper.modify(|r, w| {
+                        w.bits(
+                            (r.bits() & !(0b1 << $pin_number))
+                                | (<super::Alternate<$afi, $mode>>::TYPE << $pin_number),
+                        )
+                    });
+                    // Write the config into the pull-up/pull-down register
+                    gpio.pupdr.modify(|r, w| {
+                        w.bits(
+                            (r.bits() & !(0b11 << $pin_number * 2))
+                                | (<super::Alternate<$afi, $mode>>::PULL << $pin_number * 2),
+                        )
+                    });
+                };
+                Self {
+                    _mode: Default::default(),
+                }
+            }
+        }
+    };
+}
+
 macro_rules! gpio {
     ($GPIOX:ident, $gpiox:ident, $port_id:expr, $PXn:ident, [
-        $($PXi:ident: ($pxi:ident, $pin_number:expr $(, $MODE:ty)?),)+
+        $($PXi:ident: ($pxi:ident, $pin_number:expr, [$($afi: expr,)+], [$($afi2: expr,)+] $(, $MODE:ty)?),)+
     ]) => {
         mod $gpiox {
              use crate::rcc::{Reset, Enable};
+             use super::sealed::PinMode;
              pub struct Parts {
                     $(
                         /// Pin
@@ -270,6 +317,24 @@ macro_rules! gpio {
 
             $(
                 pub type $PXi<MODE = super::Analog> = super::Pin<$port_id, $pin_number, MODE>;
+                $(
+                    from_pin!($port_id, $pin_number, $afi, super::Analog, super::PushPull);
+                    from_pin!($port_id, $pin_number, $afi, super::Analog, super::OpenDrain);
+                    from_pin!($port_id, $pin_number, $afi, super::Input<super::Floating>, super::PushPull);
+                    from_pin!($port_id, $pin_number, $afi, super::Input<super::Floating>, super::OpenDrain);
+                    from_pin!($port_id, $pin_number, $afi, super::Input<super::PullUp>, super::PushPull);
+                    from_pin!($port_id, $pin_number, $afi, super::Input<super::PullUp>, super::OpenDrain);
+                    from_pin!($port_id, $pin_number, $afi, super::Input<super::PullDown>, super::PushPull);
+                    from_pin!($port_id, $pin_number, $afi, super::Input<super::PullDown>, super::OpenDrain);
+                    from_pin!($port_id, $pin_number, $afi, super::Output<super::PushPull>, super::PushPull);
+                    from_pin!($port_id, $pin_number, $afi, super::Output<super::PushPull>, super::OpenDrain);
+                    from_pin!($port_id, $pin_number, $afi, super::Output<super::OpenDrain>, super::PushPull);
+                    from_pin!($port_id, $pin_number, $afi, super::Output<super::OpenDrain>, super::OpenDrain);
+                    $(
+                        from_pin!($port_id, $pin_number, $afi, super::Alternate<$afi2, super::OpenDrain>, super::PushPull);
+                        from_pin!($port_id, $pin_number, $afi, super::Alternate<$afi2, super::PushPull>, super::OpenDrain);
+                    )+
+                )+
             )+
 
             impl super::GpioExt for crate::pac::$GPIOX {
@@ -292,137 +357,137 @@ macro_rules! gpio {
 }
 
 gpio!(GPIOA, gpioa, 'A', PAn, [
-    PA0: (pa0, 0),
-    PA1: (pa1, 1),
-    PA2: (pa2, 2),
-    PA3: (pa3, 3),
-    PA4: (pa4, 4),
-    PA5: (pa5, 5),
-    PA6: (pa6, 6),
-    PA7: (pa7, 7),
-    PA8: (pa8, 8),
-    PA9: (pa9, 9),
-    PA10: (pa10, 10),
-    PA11: (pa11, 11),
-    PA12: (pa12, 12),
-    PA13: (pa13, 13, super::Debugger), // SWDIO, PullUp VeryHigh speed
-    PA14: (pa14, 14, super::Debugger), // SWCLK, PullDown
-    PA15: (pa15, 15, super::Debugger), // JTDI, PullUp
+    PA0: (pa0, 0, [1,7,8,9,10,14,15,], [1,7,8,9,10,14,15,]),
+    PA1: (pa1, 1, [0,1,7,9,15,], [0,1,7,9,15,]),
+    PA2: (pa2, 2, [1,7,8,9,12,14,15,], [1,7,8,9,12,14,15,]),
+    PA3: (pa3, 3, [1,3,7,9,12,13,15,], [1,3,7,9,12,13,15,]),
+    PA4: (pa4, 4, [2,5,6,7,13,15,], [2,5,6,7,13,15,]),
+    PA5: (pa5, 5, [1,2,5,14,15,], [1,2,5,14,15,]),
+    PA6: (pa6, 6, [1,2,4,5,6,8,12,15,], [1,2,4,5,6,8,12,15,]),
+    PA7: (pa7, 7, [1,2,4,5,6,8,14,15,], [1,2,4,5,6,8,14,15,]),
+    PA8: (pa8, 8, [0,2,4,5,6,7,10,12,14,15,], [0,2,4,5,6,7,10,12,14,15,]),
+    PA9: (pa9, 9, [2,4,5,6,7,9,10,14,15,], [2,4,5,6,7,9,10,14,15,]),
+    PA10: (pa10, 10, [1,3,4,5,6,7,10,11,12,14,15,], [1,3,4,5,6,7,10,11,12,14,15,]),
+    PA11: (pa11, 11, [5,6,7,8,9,10,11,12,15,], [5,6,7,8,9,10,11,12,15,]),
+    PA12: (pa12, 12, [1,5,6,7,8,9,10,11,15,], [1,5,6,7,8,9,10,11,15,]),
+    PA13: (pa13, 13, [0,1,4,5,7,10,13,15,], [0,1,4,5,7,10,13,15,], super::Debugger), // SWDIO, PullUp VeryHigh speed
+    PA14: (pa14, 14, [0,1,4,5,6,7,13,15,],[0,1,4,5,6,7,13,15,], super::Debugger), // SWCLK, PullDown
+    PA15: (pa15, 15, [0,1,2,4,5,6,7,8,9,14,15,],[0,1,2,4,5,6,7,8,9,14,15,], super::Debugger), // JTDI, PullUp
 ]);
 
-gpio!(GPIOB, gpiob, 'B', PBn, [
-    PB0: (pb0, 0),
-    PB1: (pb1, 1),
-    PB2: (pb2, 2),
-    PB3: (pb3, 3, super::Debugger), // SWO, VeryHigh speed
-    PB4: (pb4, 4, super::Debugger), // JTRST, PullUp
-    PB5: (pb5, 5),
-    PB6: (pb6, 6),
-    PB7: (pb7, 7),
-    PB8: (pb8, 8),
-    PB9: (pb9, 9),
-    PB10: (pb10, 10),
-    PB11: (pb11, 11),
-    PB12: (pb12, 12),
-    PB13: (pb13, 13),
-    PB14: (pb14, 14),
-    PB15: (pb15, 15),
-]);
-
-gpio!(GPIOC, gpioc, 'C', PCn, [
-    PC0: (pc0, 0),
-    PC1: (pc1, 1),
-    PC2: (pc2, 2),
-    PC3: (pc3, 3),
-    PC4: (pc4, 4),
-    PC5: (pc5, 5),
-    PC6: (pc6, 6),
-    PC7: (pc7, 7),
-    PC8: (pc8, 8),
-    PC9: (pc9, 9),
-    PC10: (pc10, 10),
-    PC11: (pc11, 11),
-    PC12: (pc12, 12),
-    PC13: (pc13, 13),
-    PC14: (pc14, 14),
-    PC15: (pc15, 15),
-]);
-
-gpio!(GPIOD, gpiod, 'D', PDn, [
-    PD0: (pd0, 0),
-    PD1: (pd1, 1),
-    PD2: (pd2, 2),
-    PD3: (pd3, 3),
-    PD4: (pd4, 4),
-    PD5: (pd5, 5),
-    PD6: (pd6, 6),
-    PD7: (pd7, 7),
-    PD8: (pd8, 8),
-    PD9: (pd9, 9),
-    PD10: (pd10, 10),
-    PD11: (pd11, 11),
-    PD12: (pd12, 12),
-    PD13: (pd13, 13),
-    PD14: (pd14, 14),
-    PD15: (pd15, 15),
-]);
-
-gpio!(GPIOE, gpioe, 'E', PEn, [
-    PE0: (pe0, 0),
-    PE1: (pe1, 1),
-    PE2: (pe2, 2),
-    PE3: (pe3, 3),
-    PE4: (pe4, 4),
-    PE5: (pe5, 5),
-    PE6: (pe6, 6),
-    PE7: (pe7, 7),
-    PE8: (pe8, 8),
-    PE9: (pe9, 9),
-    PE10: (pe10, 10),
-    PE11: (pe11, 11),
-    PE12: (pe12, 12),
-    PE13: (pe13, 13),
-    PE14: (pe14, 14),
-    PE15: (pe15, 15),
-]);
-
-gpio!(GPIOF, gpiof, 'F', PFn, [
-    PF0: (pf0, 0),
-    PF1: (pf1, 1),
-    PF2: (pf2, 2),
-    PF3: (pf3, 3),
-    PF4: (pf4, 4),
-    PF5: (pf5, 5),
-    PF6: (pf6, 6),
-    PF7: (pf7, 7),
-    PF8: (pf8, 8),
-    PF9: (pf9, 9),
-    PF10: (pf10, 10),
-    PF11: (pf11, 11),
-    PF12: (pf12, 12),
-    PF13: (pf13, 13),
-    PF14: (pf14, 14),
-    PF15: (pf15, 15),
-]);
-
-gpio!(GPIOG, gpiog, 'G', PGn, [
-    PG0: (pg0, 0),
-    PG1: (pg1, 1),
-    PG2: (pg2, 2),
-    PG3: (pg3, 3),
-    PG4: (pg4, 4),
-    PG5: (pg5, 5),
-    PG6: (pg6, 6),
-    PG7: (pg7, 7),
-    PG8: (pg8, 8),
-    PG9: (pg9, 9),
-    PG10: (pg10, 10),
-    PG11: (pg11, 11),
-    PG12: (pg12, 12),
-    PG13: (pg13, 13),
-    PG14: (pg14, 14),
-    PG15: (pg15, 15),
-]);
+// gpio!(GPIOB, gpiob, 'B', PBn, [
+//     PB0: (pb0, 0),
+//     PB1: (pb1, 1),
+//     PB2: (pb2, 2),
+//     PB3: (pb3, 3, super::Debugger), // SWO, VeryHigh speed
+//     PB4: (pb4, 4, super::Debugger), // JTRST, PullUp
+//     PB5: (pb5, 5),
+//     PB6: (pb6, 6),
+//     PB7: (pb7, 7),
+//     PB8: (pb8, 8),
+//     PB9: (pb9, 9),
+//     PB10: (pb10, 10),
+//     PB11: (pb11, 11),
+//     PB12: (pb12, 12),
+//     PB13: (pb13, 13),
+//     PB14: (pb14, 14),
+//     PB15: (pb15, 15),
+// ]);
+//
+// gpio!(GPIOC, gpioc, 'C', PCn, [
+//     PC0: (pc0, 0),
+//     PC1: (pc1, 1),
+//     PC2: (pc2, 2),
+//     PC3: (pc3, 3),
+//     PC4: (pc4, 4),
+//     PC5: (pc5, 5),
+//     PC6: (pc6, 6),
+//     PC7: (pc7, 7),
+//     PC8: (pc8, 8),
+//     PC9: (pc9, 9),
+//     PC10: (pc10, 10),
+//     PC11: (pc11, 11),
+//     PC12: (pc12, 12),
+//     PC13: (pc13, 13),
+//     PC14: (pc14, 14),
+//     PC15: (pc15, 15),
+// ]);
+//
+// gpio!(GPIOD, gpiod, 'D', PDn, [
+//     PD0: (pd0, 0),
+//     PD1: (pd1, 1),
+//     PD2: (pd2, 2),
+//     PD3: (pd3, 3),
+//     PD4: (pd4, 4),
+//     PD5: (pd5, 5),
+//     PD6: (pd6, 6),
+//     PD7: (pd7, 7),
+//     PD8: (pd8, 8),
+//     PD9: (pd9, 9),
+//     PD10: (pd10, 10),
+//     PD11: (pd11, 11),
+//     PD12: (pd12, 12),
+//     PD13: (pd13, 13),
+//     PD14: (pd14, 14),
+//     PD15: (pd15, 15),
+// ]);
+//
+// gpio!(GPIOE, gpioe, 'E', PEn, [
+//     PE0: (pe0, 0),
+//     PE1: (pe1, 1),
+//     PE2: (pe2, 2),
+//     PE3: (pe3, 3),
+//     PE4: (pe4, 4),
+//     PE5: (pe5, 5),
+//     PE6: (pe6, 6),
+//     PE7: (pe7, 7),
+//     PE8: (pe8, 8),
+//     PE9: (pe9, 9),
+//     PE10: (pe10, 10),
+//     PE11: (pe11, 11),
+//     PE12: (pe12, 12),
+//     PE13: (pe13, 13),
+//     PE14: (pe14, 14),
+//     PE15: (pe15, 15),
+// ]);
+//
+// gpio!(GPIOF, gpiof, 'F', PFn, [
+//     PF0: (pf0, 0),
+//     PF1: (pf1, 1),
+//     PF2: (pf2, 2),
+//     PF3: (pf3, 3),
+//     PF4: (pf4, 4),
+//     PF5: (pf5, 5),
+//     PF6: (pf6, 6),
+//     PF7: (pf7, 7),
+//     PF8: (pf8, 8),
+//     PF9: (pf9, 9),
+//     PF10: (pf10, 10),
+//     PF11: (pf11, 11),
+//     PF12: (pf12, 12),
+//     PF13: (pf13, 13),
+//     PF14: (pf14, 14),
+//     PF15: (pf15, 15),
+// ]);
+//
+// gpio!(GPIOG, gpiog, 'G', PGn, [
+//     PG0: (pg0, 0),
+//     PG1: (pg1, 1),
+//     PG2: (pg2, 2),
+//     PG3: (pg3, 3),
+//     PG4: (pg4, 4),
+//     PG5: (pg5, 5),
+//     PG6: (pg6, 6),
+//     PG7: (pg7, 7),
+//     PG8: (pg8, 8),
+//     PG9: (pg9, 9),
+//     PG10: (pg10, 10),
+//     PG11: (pg11, 11),
+//     PG12: (pg12, 12),
+//     PG13: (pg13, 13),
+//     PG14: (pg14, 14),
+//     PG15: (pg15, 15),
+// ]);
 
 impl<const P: char, const N: u8, MODE: Default> Pin<P, N, MODE> {
     fn new() -> Self {
